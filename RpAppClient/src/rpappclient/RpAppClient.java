bb9ee82d5b52194cce0ac252ce1550dc214c40e5
Mathieu <Mathieu@192.168.0.109>
2012-06-25 19:32:05 -0400

    Création d'un programme pour tester le serveur (Envoy de donnée simple)

--- RpAppClient/src/rpappclient/RpAppClient.java
+++ RpAppClient/src/rpappclient/RpAppClient.java
@@ -4,6 +4,15 @@
  */
 package rpappclient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mathieu
@@ -14,6 +23,27 @@ public class RpAppClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+
+            // TODO code application logic here
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("localhost", 44910));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            for (int i = 0; i < 5; i++) {
+                out.println("Test de Data");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(RpAppClient.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException ex) {
+        }
+
     }
 }
